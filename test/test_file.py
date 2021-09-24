d6bd9338a4c599a079bdf55e02477dce7928d3eb
Happy Singh <hsingh0805@gmail.com>
2021-09-24 23:27:10 +0530

    tests added

---
+++ test/test_file.py
@@ -0,0 +1,4 @@
+import test_main
+
+def test_data_file_present():
+    assert test_main.data_file_present() == False
---
+++ test_main.py
@@ -0,0 +1,4 @@
+import os
+
+def data_file_present():
+    return os.path.exists('./data.zip')
