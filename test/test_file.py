726cc544315dffafa82a1300b6ef7db4bf8af638
Happy Singh <hsingh0805@gmail.com>
2021-09-24 23:33:16 +0530

    check .h5 file

--- test/test_file.py
+++ test/test_file.py
@@ -2,3 +2,6 @@ import test_main
 
 def test_data_file_present():
     assert test_main.data_file_present() == False
+
+def test_is_model_present():
+    assert test_main.is_model_present() == False
--- test_main.py
+++ test_main.py
@@ -2,3 +2,6 @@ import os
 
 def data_file_present():
     return os.path.exists('./data.zip')
+
+def is_model_present():
+    return os.path.exists('./model.h5')
